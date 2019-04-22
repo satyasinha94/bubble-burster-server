be7f324da11c5e59e06776587efb347336401f31
satyasinha94 <satyasinha94@gmail.com>
2019-04-22 00:44:10 -0400

    cleaned up artist serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -2,5 +2,5 @@ class ArtistSerializer < ActiveModel::Serializer
   attributes :name, :spotify_url, :href, :spotify_id, :uri, :img_url
   has_many :tracks
   has_many :genres
-  has_many :user_artists, key: :UserArtists
+  has_many :user_artists
 end
