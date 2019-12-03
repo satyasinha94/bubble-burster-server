51baa35221e55923f434f4cafbe38bbaacf0d418
satyasinha94 <satyasinha94@gmail.com>
2019-12-03 00:16:22 -0500

    removed dummy test

--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -4,15 +4,15 @@ class Api::V1::RecommendationsController < ApplicationController
   end
 
   def tracks
-    render json: Recommendation.top_tracks_track_recs(curr_user).sample(20)
+    render json: Recommendation.top_tracks_track_recs(curr_user).sample(15)
   end
 
   def artists
-    render json: Recommendation.top_artist_track_recs(curr_user).sample(20)
+    render json: Recommendation.top_artist_track_recs(curr_user).sample(15)
   end
 
   def genres
-    render json: Recommendation.genre_recs(curr_user).sample(20)
+    render json: Recommendation.genre_recs(curr_user).sample(15)
   end
 
 end
--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 require 'user'
 
-describe User do 
-    describe '#expired' do
-        it "does nothing" do
-            expect(User.first.expired).to eql(nil)
-        end
-    end
-end
+# TEST to make sure pre-commit hook works
+# describe User do 
+#     describe '#expired' do
+#         it "does nothing" do
+#             expect(User.first.expired).to eql(nil)
+#         end
+#     end
+# end
